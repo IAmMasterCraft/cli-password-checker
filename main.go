ec978e0e4eaa86b1c8804f8281337d687e98d11c
IAmMasterCraft <bolu.akinsefunmi@gmail.com>
2024-04-23 13:53:46 +0100

    improved error handling

--- main.go
+++ main.go
@@ -10,74 +10,83 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"errors"
 )
 
 func main() {
 	fmt.Println("Enter a password to check:")
-	input := bufio.NewScanner(os.Stdin)
-	if input.Scan() {
-		password := input.Text()
-		score := checkPasswordStrength(password)
-		percentage := calculatePercentage(score, 20)
-		fmt.Printf("Password Strength Evaluation: %d%%\n", percentage)
-		printPasswordStrength(score)
-	}
-	if err := input.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading standard input from user:", err)
-	}
+    input := bufio.NewScanner(os.Stdin)
+    if input.Scan() {
+        password := input.Text()
+        score, err := checkPasswordStrength(password)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "Error checking password strength: %v\n", err)
+            return
+        }
+        percentage := (score * 100) / 20
+        fmt.Printf("Password Strength Evaluation: %d%%\n", percentage)
+        classifyPassword(score)
+    }
+    if err := input.Err(); err != nil {
+        fmt.Fprintf(os.Stderr, "Error reading standard input: %v\n", err)
+    }
 }
 
-func checkPasswordStrength(password string) int {
-	score := 0
-	charTypesPresent := 0
-	passwordLength := len(password)
-
-	if passwordLength >= 12 {
-		score += 4
-	} else if passwordLength >= 8 {
-		score += 2
+func checkPasswordStrength(password string) (int, error) {
+	score, err := evaluatePassword(password)
+	if err != nil {
+		return 0, fmt.Errorf("error evaluating password: %w", err)
 	}
 
-	// uppercase, lowercase, number & special character
-	hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar := checkCharTypes(password)
-
-	if hasUpperCase {
-		charTypesPresent++
-	}
-	if hasLowerCase {
-		charTypesPresent++
-	}
-	if hasDigit {
-		charTypesPresent++
-	}
-	if hasSpecialChar {
-		charTypesPresent++
-	}
-
-	switch charTypesPresent {
-	case 1:
-		score++
-	case 2:
-		score += 3
-	case 3:
-		score += 5
-	case 4:
-		score += 8
-	}
-
-	if hasRepeatedChars(password) {
-		score -= 2
-	}
-
-	// penalty for compromised password
 	compromised, err := isCompromised(password)
 	if err != nil {
-		fmt.Println("Error checking for compromised password:", err)
+		return score, fmt.Errorf("error checking if password is compromised: %w", err)
 	} else if compromised {
 		score -= 3
 	}
 
-	return score
+	return score, nil
+}
+
+func evaluatePassword(password string) (int, error) {
+    score := 0
+    typesPresent := 0
+
+    hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar := checkCharTypes(password)
+    if hasUpperCase {
+        typesPresent++
+    }
+    if hasLowerCase {
+        typesPresent++
+    }
+    if hasDigit {
+        typesPresent++
+    }
+    if hasSpecialChar {
+        typesPresent++
+    }
+
+    score += calculateDiversityScore(typesPresent)
+
+    if hasRepeatedChars(password) {
+        return score, errors.New("password has repeated characters")
+    }
+    return score, nil
+}
+
+func calculateDiversityScore(typesPresent int) int {
+    switch typesPresent {
+    case 1:
+        return 1
+    case 2:
+        return 3
+    case 3:
+        return 5
+    case 4:
+        return 8
+    default:
+        return 0
+    }
 }
 
 func checkCharTypes(password string) (bool, bool, bool, bool) {
@@ -123,30 +132,30 @@ func isCompromised(password string) (bool, error) {
 	url := fmt.Sprintf("https://api.pwnedpasswords.com/range/%s", prefix)
 	response, err := http.Get(url)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("HTTP request failed: %w", err)
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return false, err
+		return false, fmt.Errorf("failed to read HTTP response: %w", err)
 	}
 
-	hashes := strings.Split(string(body), "\r\n")
-	for _, line := range hashes {
-		parts := strings.Split(line, ":")
-		if len(parts) > 0 && strings.ToUpper(parts[0]) == suffix {
-			return true, nil
-		}
-	}
-	return false, nil
+	return checkHash(suffix, body), nil
 }
 
-func calculatePercentage(score int, length int) int {
-	return (score * 100) / length
+func checkHash(suffix string, body []byte) bool {
+    hashes := strings.Split(string(body), "\r\n")
+    for _, line := range hashes {
+        parts := strings.Split(line, ":")
+        if len(parts) > 0 && strings.ToUpper(parts[0]) == suffix {
+            return true
+        }
+    }
+    return false
 }
 
-func printPasswordStrength(score int) {
+func classifyPassword(score int) {
 	switch {
 	case score < 5:
 		fmt.Println("Password is weak.")
@@ -155,306 +164,4 @@ func printPasswordStrength(score int) {
 	default:
 		fmt.Println("Password is strong.")
 	}
-}
-
-func checkPasswordStrength(password string) int {
-	score := 0
-	charTypesPresent := 0
-	passwordLength := len(password)
-
-	// if password length is 8 reward with 1 point if it is greater than or equal to 12 reward with 2 points
-	if passwordLength >= 12 {
-		score += 4
-	} else if passwordLength >= 8 {
-		score += 2
-	}
-
-	// uppercase, lowercase, number & special character
-	hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar := checkCharTypes(password)
-
-	if hasUpperCase {
-		charTypesPresent++
-	}
-	if hasLowerCase {
-		charTypesPresent++
-	}
-	if hasDigit {
-		charTypesPresent++
-	}
-	if hasSpecialChar {
-		charTypesPresent++
-	}
-
-	switch charTypesPresent {
-	case 1:
-		score++
-	case 2:
-		score += 3
-	case 3:
-		score += 5
-	case 4:
-		score += 8
-	}
-
-	if hasRepeatedChars(password) {
-		score -= 2
-	}
-
-	// penalty for compromised password
-	compromised, err := isCompromised(password)
-	if err != nil {
-		fmt.Println("Error checking for compromised password:", err)
-	} else if compromised {
-		score -= 3
-	}
-
-	return score
-}
-
-func checkCharTypes(password string) (bool, bool, bool, bool) {
-	var hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar bool
-	for _, char := range password {
-		if unicode.IsUpper(char) {
-			hasUpperCase = true
-		}
-		if unicode.IsLower(char) {
-			hasLowerCase = true
-		}
-		if unicode.IsDigit(char) {
-			hasDigit = true
-		}
-		if unicode.IsPunct(char) || unicode.IsSymbol(char) {
-			hasSpecialChar = true
-		}
-	}
-	return hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar
-}
-
-func hasRepeatedChars(password string) bool {
-	var previous rune
-	for i, char := range password {
-		if i > 0 && char == previous {
-			return true
-		}
-		previous = char
-	}
-	return false
-}
-
-func sha1Hash(text string) string {
-	hasher := sha1.New()
-	hasher.Write([]byte(text))
-	return strings.ToUpper(hex.EncodeToString(hasher.Sum(nil)))
-}
-
-func isCompromised(password string) (bool, error) {
-	hash := sha1Hash(password)
-	prefix := hash[:5]
-	suffix := hash[5:]
-	url := fmt.Sprintf("https://api.pwnedpasswords.com/range/%s", prefix)
-	response, err := http.Get(url)
-	if err != nil {
-		return false, err
-	}
-	defer response.Body.Close()
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return false, err
-	}
-
-	hashes := strings.Split(string(body), "\r\n")
-	for _, line := range hashes {
-		parts := strings.Split(line, ":")
-		if len(parts) > 0 && strings.ToUpper(parts[0]) == suffix {
-			return true, nil
-		}
-	}
-	return false, nil
-}
-
-import (
-	"fmt"
-)
-
-func checkPasswordStrength(password string) int {
-	score := 0
-	charTypesPresent := 0
-	passwordLength := len(password)
-
-	// if password length is 8 reward with 1 point if it is greaer than or equal to 12 reward with 2 points
-	if passwordLength >= 12 {
-		score += 4
-	} else if passwordLength >= 8 {
-		score += 2
-	}
-	
-	// uppercase, lowercase, number & special character
-	hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar := checkCharTypes(password)
-
-	if hasUpperCase {
-		charTypesPresent++
-	}
-	if hasLowerCase {
-		charTypesPresent++
-	}
-	if hasDigit {
-		charTypesPresent++
-	}
-	if hasSpecialChar {
-		charTypesPresent++
-	}
-
-	switch charTypesPresent {
-	case 1:
-		score++
-	case 2:
-		score += 3
-	case 3:
-		score += 5
-	case 4:
-		score += 8
-	}
-
-	if hasRepeatedChars(password) {
-		score -= 2
-	}
-
-	// penalty for compromised password
-	compromised, err := isCompromised(password)
-	if err != nil {
-		fmt.Println("Error checking for compromised password:", err)
-	} else if compromised {
-		score -= 3
-	}
-
-
-	return score
-}
-
-
-func checkCharTypes(password string) (bool, bool, bool, bool) {
-	var hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar bool
-	for _, char := range password {
-		if unicode.IsUpper(char) {
-			hasUpperCase = true
-		}
-	}
-
-func checkPasswordStrength(password string) int {
-	score := 0
-	charTypesPresent := 0
-	passwordLength := len(password)
-
-	// if password length is 8 reward with 1 point if it is greaer than or equal to 12 reward with 2 points
-	if passwordLength >= 12 {
-		score += 4
-	} else if passwordLength >= 8 {
-		score += 2
-	}
-	
-	// uppercase, lowercase, number & special character
-	hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar := checkCharTypes(password)
-
-	if hasUpperCase {
-		charTypesPresent++
-	}
-	if hasLowerCase {
-		charTypesPresent++
-	}
-	if hasDigit {
-		charTypesPresent++
-	}
-	if hasSpecialChar {
-		charTypesPresent++
-	}
-
-	switch charTypesPresent {
-	case 1:
-		score++
-	case 2:
-		score += 3
-	case 3:
-		score += 5
-	case 4:
-		score += 8
-	}
-
-	if hasRepeatedChars(password) {
-		score -= 2
-	}
-
-	// penalty for compromised password
-	compromised, err := isCompromised(password)
-	if err != nil {
-		fmt.Println("Error checking for compromised password:", err)
-	} else if compromised {
-		score -= 3
-	}
-
-
-	return score
-}
-
-
-func checkCharTypes(password string) (bool, bool, bool, bool) {
-	var hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar bool
-	for _, char := range password {
-		if unicode.IsUpper(char) {
-			hasUpperCase = true
-		}
-		if unicode.IsLower(char) {
-			hasLowerCase = true
-		}
-		if unicode.IsDigit(char) {
-			hasDigit = true
-		}
-		if unicode.IsPunct(char) || unicode.IsSymbol(char) {
-			hasSpecialChar = true
-		}
-	}
-	return hasUpperCase, hasLowerCase, hasDigit, hasSpecialChar
-}
-
-func hasRepeatedChars(password string) bool {
-	var previous rune
-	for i, char := range password {
-		if i > 0 && char == previous {
-			return true
-		}
-		previous = char
-	}
-	return false
-}
-
-func sha1Hash(text string) string {
-	hasher := sha1.New()
-	hasher.Write([]byte(text))
-	return strings.ToUpper(hex.EncodeToString(hasher.Sum(nil)))
-}
-
-func isCompromised(password string) (bool, error) {
-	hash := sha1Hash(password)
-	prefix := hash[:5]
-	suffix := hash[5:]
-	url := fmt.Sprintf("https://api.pwnedpasswords.com/range/%s", prefix)
-	response, err := http.Get(url)
-	if err != nil {
-		return false, err
-	}
-	defer response.Body.Close()
-
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		return false, err
-	}
-
-	hashes := strings.Split(string(body), "\r\n")
-	for _, line := range hashes {
-		parts := strings.Split(line, ":")
-		if len(parts) > 0 && strings.ToUpper(parts[0]) == suffix {
-			return true, nil
-		}
-	}
-	return false, nil
 }
